ddfc360a9ea5fdc6a2253206da69e39994277f85
segundo_acosta <sacosta@platcel.com>
2016-11-05 13:15:46 -0500

    animo

---
+++ clase02/clase02/app/src/main/java/area51/clase02/screen/launcher/view/LauncherActivity.java
@@ -0,0 +1,15 @@
+package area51.clase02.screen.launcher.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import area51.clase02.R;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
