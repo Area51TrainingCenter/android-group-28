8f88b5278cad09de7098233f7d7b4f9d6968deb0
segundo_acosta <sacosta@platcel.com>
2016-11-12 10:02:11 -0500

    Clase 03

---
+++ clase03/clase02/app/src/main/java/area51/clase02/screen/welcome/model/Item.java
@@ -0,0 +1,47 @@
+package area51.clase02.screen.welcome.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by segundo on 5/11/16.
+ */
+
+public class Item implements Serializable {
+
+    private String id;
+    private String image;
+    private String name;
+    private String stock;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+}
---
+++ clase03/clase02/app/src/main/java/area51/clase02/screen/welcome/view/ItemAdapter.java
@@ -0,0 +1,83 @@
+package area51.clase02.screen.welcome.view;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.ArrayList;
+
+import area51.clase02.R;
+import area51.clase02.screen.welcome.model.Item;
+
+
+/**
+ * Created by segundo on 5/11/16.
+ */
+
+public class ItemAdapter extends ArrayAdapter<Item> {
+
+    Context context;
+
+    public ItemAdapter(Context context, ArrayList<Item> list) {
+        super(context, 0, list);
+        this.context = context;
+    }
+
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+
+        ViewHolder holder;
+
+        if (view == null) {
+
+            holder = new ViewHolder();
+            view = LayoutInflater
+                    .from(context)
+                    .inflate(R.layout.row_item, parent, false);
+
+            holder.name = (TextView) view.findViewById(R.id.name);
+            holder.stock = (TextView) view.findViewById(R.id.stock);
+            holder.image = (SimpleDraweeView) view.findViewById(R.id.image);
+
+            view.setTag(holder);
+
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+
+        Item item = getItem(position);
+
+        holder.name.setText(item.getName());
+        holder.stock.setText(item.getStock());
+
+        //Agregamos una imagen usando Fresco-lib
+        Uri uri = Uri.parse("res:///" + R.mipmap.ic_launcher);
+
+        DraweeController controller = Fresco.newDraweeControllerBuilder()
+                .setUri(uri)
+                .setTapToRetryEnabled(true)
+                .setOldController(holder.image.getController())
+                .build();
+
+        holder.image.setController(controller);
+
+        return view;
+    }
+
+
+    static class ViewHolder {
+        TextView name, stock;
+        SimpleDraweeView image;
+    }
+
+}
