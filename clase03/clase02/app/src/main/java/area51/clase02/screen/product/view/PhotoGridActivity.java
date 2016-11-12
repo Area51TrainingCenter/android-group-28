7587111583cf784cef714778bcf49da18c3e4938
segundo_acosta <sacosta@platcel.com>
2016-11-12 14:46:34 -0500

    PhotoGrid

---
+++ clase03/clase02/app/src/main/java/area51/clase02/screen/product/view/PhotoGridActivity.java
@@ -0,0 +1,15 @@
+package area51.clase02.screen.product.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import area51.clase02.R;
+
+public class PhotoGridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_grid);
+    }
+}
