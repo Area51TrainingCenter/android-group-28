c7e82c6101e78677e0b7d57cb4be2fe70e2a59cf
segundo_acosta <sacosta@platcel.com>
2016-12-03 13:56:44 -0500

    add button

--- PracticaM1/app/src/main/java/area51/practicam1/screen/home/view/MainActivity.java
+++ PracticaM1/app/src/main/java/area51/practicam1/screen/home/view/MainActivity.java
@@ -1,15 +1,47 @@
 package area51.practicam1.screen.home.view;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import area51.practicam1.PracticaGlobals;
 import area51.practicam1.R;
+import area51.practicam1.model.Pokemon;
 
 public class MainActivity extends AppCompatActivity {
+
+    PokemonAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView list = (ListView) findViewById(R.id.list);
+
+        getItem();
+        adapter = new PokemonAdapter(this, PracticaGlobals.pokemons);
+
+        list.setAdapter(adapter);
+
+    }
+
+
+    void getItem() {
+
+        PracticaGlobals.pokemons = new ArrayList<Pokemon>();
+
+        for (int i = 0; i < 10; i++) {
+            Pokemon pokemon = new Pokemon();
+            pokemon.setName("Pokemon " + i);
+            pokemon.setType("Tipo" + i);
+
+            //PracticaGlobals.pokemons.add(pokemon);
+        }
+
+
     }
 }
--- PracticaM1/app/src/main/java/area51/practicam1/screen/home/view/PokemonAdapter.java
+++ PracticaM1/app/src/main/java/area51/practicam1/screen/home/view/PokemonAdapter.java
@@ -1,10 +1,17 @@
 package area51.practicam1.screen.home.view;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import area51.practicam1.R;
 import area51.practicam1.model.Pokemon;
 
 /**
@@ -17,9 +24,41 @@ public class PokemonAdapter extends ArrayAdapter<Pokemon> {
 
     public PokemonAdapter(Context context, ArrayList<Pokemon> list) {
         super(context, 0, list);
+        this.context = context;
     }
 
+    @NonNull
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+
+        ViewHolder vh;
+
+        if (view == null) {
+            vh = new ViewHolder();
+
+            view = LayoutInflater.from(context)
+                    .inflate(R.layout.row_pokemon, parent, false);
+
+            vh.image = (ImageView) view.findViewById(R.id.image);
+            vh.name = (TextView) view.findViewById(R.id.name);
+            vh.type = (TextView) view.findViewById(R.id.type);
+
+            view.setTag(vh);
+
+        } else {
+            vh = (ViewHolder) view.getTag();
+        }
+
+        vh.name.setText(getItem(position).getName());
+        vh.type.setText(getItem(position).getType());
 
 
+        return view;
+    }
+
+    static class ViewHolder {
+        ImageView image;
+        TextView name, type;
+    }
 
 }
