429c1dd29a1ca15ff06ca9ae1917fda703ad7037
segundo_acosta <sacosta@platcel.com>
2016-12-03 12:28:52 -0500

    Practica

---
+++ PracticaM1/app/src/main/java/area51/practicam1/model/Pokemon.java
@@ -0,0 +1,13 @@
+package area51.practicam1.model;
+
+/**
+ * Created by segundo on 3/12/16.
+ */
+
+public class Pokemon {
+
+    String name;
+    String type;
+    int image;
+    
+}
---
+++ PracticaM1/app/src/main/java/area51/practicam1/screen/home/view/MainActivity.java
@@ -0,0 +1,15 @@
+package area51.practicam1.screen.home.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import area51.practicam1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
