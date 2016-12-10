bd9b7fe5ff8b2ae96eed53959d02d27703423392
segundo_acosta <sacosta@platcel.com>
2016-12-10 14:57:24 -0500

    add People

---
+++ Module02/Clase05/Clase05/app/src/main/java/area51/clase05/model/People.java
@@ -0,0 +1,116 @@
+package area51.clase05.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by segundo on 10/12/16.
+ */
+
+public class People implements Serializable {
+
+    private String gender;
+    //String name;
+    private String title;
+    private String first;
+    private String last;
+    //Location
+    private String city;
+    private String state;
+    //Email
+    private String email;
+    //Phone
+    private String registered;
+    private String phone;
+    private String cell;
+    //Picture
+    private String picture_large;
+
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(String registered) {
+        this.registered = registered;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCell() {
+        return cell;
+    }
+
+    public void setCell(String cell) {
+        this.cell = cell;
+    }
+
+    public String getPicture_large() {
+        return picture_large;
+    }
+
+    public void setPicture_large(String picture_large) {
+        this.picture_large = picture_large;
+    }
+}
