cfe17f24ab1dec65f6af435fc78400dead5533bd
segundo_acosta <sacosta@platcel.com>
2016-12-10 11:03:25 -0500

    libraries

---
+++ Module02/Clase05/Clase05/app/src/main/java/area51/clase05/screens/launcher/view/LauncherActivity.java
@@ -0,0 +1,15 @@
+package area51.clase05.screens.launcher.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import area51.clase05.R;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
