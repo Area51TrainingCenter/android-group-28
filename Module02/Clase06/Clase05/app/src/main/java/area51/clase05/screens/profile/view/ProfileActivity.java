870bcaca749aa7fbc23e7547f45ddf8a7c6bdbdc
segundo_acosta <sacosta@platcel.com>
2016-12-17 15:22:38 -0500

    add photo

---
+++ Module02/Clase06/Clase05/app/src/main/java/area51/clase05/screens/profile/view/ProfileActivity.java
@@ -0,0 +1,16 @@
+package area51.clase05.screens.profile.view;
+
+
+import android.os.Bundle;
+
+import area51.clase05.R;
+import area51.clase05.libraries.widgets.activity.Clase05Activity;
+
+public class ProfileActivity extends Clase05Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
---
+++ Module02/Clase06/Clase05/app/src/main/java/area51/clase05/screens/profile/viewmodel/ProfileViewModel.java
@@ -0,0 +1,8 @@
+package area51.clase05.screens.profile.viewmodel;
+
+/**
+ * Created by segundo on 17/12/16.
+ */
+
+public class ProfileViewModel {
+}
