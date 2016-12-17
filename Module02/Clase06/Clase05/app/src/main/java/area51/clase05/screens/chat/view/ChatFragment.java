30210aff756e94a552d2012998b9e84fd62eed85
segundo_acosta <sacosta@platcel.com>
2016-12-17 11:07:48 -0500

    add chats screen

--- Module02/Clase06/Clase05/app/src/main/java/area51/clase05/screens/chat/view/ChatFragment.java
+++ Module02/Clase06/Clase05/app/src/main/java/area51/clase05/screens/chat/view/ChatFragment.java
@@ -1,18 +1,12 @@
 package area51.clase05.screens.chat.view;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
-
-import java.util.ArrayList;
 
 import area51.clase05.R;
-import area51.clase05.model.People;
-import area51.clase05.screens.calls.view.CallsAdapter;
 
 public class ChatFragment extends Fragment {
 
@@ -26,7 +20,7 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.fragment_calls, container, false);
+        view = inflater.inflate(R.layout.fragment_chat, container, false);
 
 
 
