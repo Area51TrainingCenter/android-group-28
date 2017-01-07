2ba71c7d4fa080e40bd1e6aed2945a166a104b34
segundo_acosta <sacosta@platcel.com>
2017-01-07 13:14:31 -0500

    Notifications

--- Module02/Clase06/Clase05/app/src/main/java/area51/clase05/screens/profile/viewmodel/ProfileViewModel.java
+++ Module02/Clase06/Clase05/app/src/main/java/area51/clase05/screens/profile/viewmodel/ProfileViewModel.java
@@ -2,9 +2,25 @@ package area51.clase05.screens.profile.viewmodel;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import area51.clase05.Clase05Application;
+import area51.clase05.Clase05Globals;
+import area51.clase05.R;
 import area51.clase05.databinding.ActivityProfileBinding;
 import area51.clase05.libraries.fresco.TrackingImage;
+import area51.clase05.libraries.log.TrackingLog;
 import area51.clase05.model.People;
 
 
@@ -44,6 +60,79 @@ public class ProfileViewModel {
         binding.description.setText(people.getName());
         binding.username.setText(people.getEmail());
         binding.mobile.setText(people.getCell());
+
+        getMedia();
+
+    }
+
+    void getMedia() {
+
+        String url = Clase05Globals.rest_calls +
+                Clase05Globals.par_results + Clase05Globals.API_ROWS;
+
+        TrackingLog.getLog("url: " + url);
+
+        JsonObjectRequest jo = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        TrackingLog.getLog("response: " + response);
+
+                        try {
+
+                            if (response.has(Clase05Globals.req_results)) {
+
+                                JSONArray data = response.getJSONArray(Clase05Globals.req_results);
+
+                                int total = data.length();
+                                JSONObject item = null;
+
+                                for (int i = 0; i < total; i++) {
+
+                                    item = data.getJSONObject(i);
+
+                                    JSONObject picture = item.getJSONObject(Clase05Globals.req_picture);
+                                    String picture_large = picture.getString(Clase05Globals.req_large);
+
+                                    View view = LayoutInflater.from(context)
+                                            .inflate(R.layout.row_media, binding.media, false);
+
+                                    SimpleDraweeView image = (SimpleDraweeView)view.findViewById(R.id.photo);
+
+                                    TrackingImage imageLoader = new TrackingImage(context);
+                                    imageLoader.view = image;
+                                    imageLoader.url = picture_large;
+                                    imageLoader.showImage();
+
+                                    binding.media.addView(view);
+
+                                }
+
+                            } else {
+                                //No hay datos
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        TrackingLog.getLog("error: " + error.getMessage());
+                    }
+                }
+        );
+
+
+        Clase05Application.getInstance().addToRequestQueue(jo, Clase05Globals.TAG_JSON_OBJ);
 
 
     }
---
+++ Module02/Clase08/Notificaciones/app/src/main/java/area51/notificaciones/sad/MainActivity.java
@@ -0,0 +1,15 @@
+package area51.notificaciones.sad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_main);
+
+    }
+}
