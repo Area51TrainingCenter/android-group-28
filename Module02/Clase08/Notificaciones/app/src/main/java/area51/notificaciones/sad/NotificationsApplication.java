189840808c730f5065ca226328cbb0a8da3aa146
segundo_acosta <sacosta@platcel.com>
2017-01-14 12:21:42 -0500

    videoplayer

--- Module02/Clase08/Notificaciones/app/src/main/java/area51/notificaciones/sad/NotificationsApplication.java
+++ Module02/Clase08/Notificaciones/app/src/main/java/area51/notificaciones/sad/NotificationsApplication.java
@@ -3,6 +3,8 @@ package area51.notificaciones.sad;
 import android.app.Application;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by segundo on 7/01/17.
@@ -13,6 +15,7 @@ public class NotificationsApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         Fresco.initialize(this);
     }
