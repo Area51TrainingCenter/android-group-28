842de5973ed0e8b2ba1cd372ac53b127715e56f1
segundo_acosta <sacosta@platcel.com>
2017-01-21 13:02:26 -0500

    add data user

--- Module02/Clase09/VideoPlayer/app/src/main/java/area51/videoplayer/screens/welcome/view/MainActivity.java
+++ Module02/Clase09/VideoPlayer/app/src/main/java/area51/videoplayer/screens/welcome/view/MainActivity.java
@@ -25,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         view = new WelcomeViewModel(this, this, binding);
         binding.setViewWelcome(view);
 
+        view.initView();
+
 
     }
 
--- Module02/Clase09/VideoPlayer/app/src/main/java/area51/videoplayer/screens/welcome/viewmodel/WelcomeViewModel.java
+++ Module02/Clase09/VideoPlayer/app/src/main/java/area51/videoplayer/screens/welcome/viewmodel/WelcomeViewModel.java
@@ -1,6 +1,11 @@
 package area51.videoplayer.screens.welcome.viewmodel;
 
+import android.app.Activity;
+import android.content.Context;
+
 import area51.videoplayer.databinding.ActivityMainBinding;
+import area51.videoplayer.libraries.images.TrackingImage;
+import area51.videoplayer.libraries.session.SessionManager;
 import area51.videoplayer.screens.welcome.view.MainActivity;
 
 /**
@@ -9,10 +14,36 @@ import area51.videoplayer.screens.welcome.view.MainActivity;
 
 public class WelcomeViewModel {
 
+    ActivityMainBinding binding;
+
+    Activity activity;
+    Context context;
+
+    SessionManager session;
 
 
+    public WelcomeViewModel(Activity activity, Context context,
+                            ActivityMainBinding binding) {
 
-    public WelcomeViewModel(MainActivity mainActivity, MainActivity mainActivity1, ActivityMainBinding binding) {
+        this.activity = activity;
+        this.context = context;
+        this.binding = binding;
+
 
     }
+
+    public void initView() {
+
+        session = new SessionManager(context);
+
+        TrackingImage loader = new TrackingImage(context);
+        loader.view = binding.avatar;
+        loader.url = session.getFoto();
+        loader.showImage();
+
+        binding.name.setText(session.getNombreUsuario());
+
+
+    }
+
 }
