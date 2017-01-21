3d87ad6793ef5f453578b0a1e0dff0d83d58dd7c
segundo_acosta <sacosta@platcel.com>
2017-01-21 14:32:07 -0500

    add listeners

--- Module02/Clase09/VideoPlayer/app/src/main/java/area51/videoplayer/screens/player/view/VideoPlayerActivity.java
+++ Module02/Clase09/VideoPlayer/app/src/main/java/area51/videoplayer/screens/player/view/VideoPlayerActivity.java
@@ -1,15 +1,80 @@
 package area51.videoplayer.screens.player.view;
 
+import android.databinding.DataBindingUtil;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 import area51.videoplayer.R;
+import area51.videoplayer.databinding.ActivityVideoPlayerBinding;
+import area51.videoplayer.screens.player.viewmodel.VideoViewModel;
 
-public class VideoPlayerActivity extends AppCompatActivity {
+public class VideoPlayerActivity extends AppCompatActivity
+        implements MediaPlayer.OnPreparedListener,
+        MediaPlayer.OnCompletionListener,
+        MediaPlayer.OnVideoSizeChangedListener,
+        SeekBar.OnSeekBarChangeListener,
+        MediaPlayer.OnErrorListener,
+        MediaPlayer.OnInfoListener {
+
+
+    ActivityVideoPlayerBinding binding;
+    VideoViewModel view;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video_player);
+        initBinding();
+    }
+
+    void initBinding() {
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_video_player);
+        view = new VideoViewModel(this, this, binding);
+        binding.setViewPlayer(view);
+        view.initView();
+
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public boolean onInfo(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
--- Module02/Clase09/VideoPlayer/app/src/main/java/area51/videoplayer/screens/player/viewmodel/VideoViewModel.java
+++ Module02/Clase09/VideoPlayer/app/src/main/java/area51/videoplayer/screens/player/viewmodel/VideoViewModel.java
@@ -1,8 +1,35 @@
 package area51.videoplayer.screens.player.viewmodel;
 
+import android.app.Activity;
+import android.content.Context;
+
+import area51.videoplayer.databinding.ActivityVideoPlayerBinding;
+import area51.videoplayer.screens.player.view.VideoPlayerActivity;
+
 /**
  * Created by segundo on 21/01/17.
  */
 
 public class VideoViewModel {
+
+    ActivityVideoPlayerBinding binding;
+
+    Activity activity;
+    Context context;
+
+
+    public VideoViewModel(Activity activity, Context context,
+                          ActivityVideoPlayerBinding binding) {
+
+        this.activity = activity;
+        this.context = context;
+        this.binding = binding;
+
+    }
+
+    public void initView() {
+
+    }
+
+
 }
