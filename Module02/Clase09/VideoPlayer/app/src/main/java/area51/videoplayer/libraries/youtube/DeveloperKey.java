b79ddd79aa4474e0ccf746cb698045250969811e
segundo_acosta <sacosta@platcel.com>
2017-02-04 10:55:36 -0500

    add youtube player

---
+++ Module02/Clase09/VideoPlayer/app/src/main/java/area51/videoplayer/libraries/youtube/DeveloperKey.java
@@ -0,0 +1,18 @@
+// Copyright 2012 Google Inc. All Rights Reserved.
+
+package area51.videoplayer.libraries.youtube;
+
+/**
+ * Static container class for holding a reference to your YouTube Developer Key.
+ */
+public class DeveloperKey {
+
+    /**
+     * Please replace this with a valid API key which is enabled for the
+     * YouTube Data API v3 service. Go to the
+     * <a href="https://code.google.com/apis/console/">Google APIs Console</a> to
+     * register a new developer key.
+     */
+    public static final String DEVELOPER_KEY = "AIzaSyB13bNZKB1gwKNb24I_RFcqD8XhEuEWHMw";
+
+}
